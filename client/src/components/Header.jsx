d11af48ce7539eb42dcf81d3469d6445ce6365d6
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-03-14 22:52:26 +0530

    complete sign up page Ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,24 +3,23 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='bg-slate-200'>
+        <div className='bg-black text-white'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
-                <h1 className='font-bold'>
-                Auth App
-                </h1>
+                    <h1 className='font-bold'>
+                        Auth App
+                    </h1>
                 </Link>
                 <ul className='flex gap-4'>
-                <Link to='/'>
-                <li>Home</li>
-                </Link>
-                <Link to='/about'>
-                <li>About</li>
-                </Link>
-                <Link to='/sign-in'>
-                <li>Sign In</li>
-                </Link>
-                    
+                    <Link to='/'>
+                        <li>Home</li>
+                    </Link>
+                    <Link to='/about'>
+                        <li>About</li>
+                    </Link>
+                    <Link to='/sign-in'>
+                        <li>Sign In</li>
+                    </Link>
                 </ul>
             </div>
         </div>
