ffd18d52f6edf18acbbcbf70ee6422490c51f734
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-03-17 18:27:06 +0530

    complete user authenticastion

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,11 +3,13 @@ import { GoogleAuthProvider, signInWithPopup , getAuth } from 'firebase/auth';
 import { app } from  '../firebase'
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from  '../redux/user/userSlice';
+import { useNavigate } from  "react-router-dom";
 
 
 
 export default function OAuth(){
     const dispatch=useDispatch()
+    const navigate=useNavigate();
     const handleGoogleClick=async () =>{
         try {
             const provider = new  GoogleAuthProvider();
