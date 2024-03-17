f09101ec325370adf2bea177a256f1d69a7104c8
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-03-17 17:08:33 +0530

    update header components and profile privarte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
-export default function App(){
+export default function App() {
   return (
     <BrowserRouter>
       {/* header */}
@@ -16,9 +17,10 @@ export default function App(){
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user);
     return (
         <div className='bg-black text-white'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -17,8 +19,12 @@ const Header = () => {
                     <Link to='/about'>
                         <li>About</li>
                     </Link>
-                    <Link to='/sign-in'>
-                        <li>Sign In</li>
+                    <Link to='/profile'>
+                    {currentUser ? (
+                    <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+                    ) : (
+                    <li>Sign In</li>
+                    )}
                     </Link>
                 </ul>
             </div>
